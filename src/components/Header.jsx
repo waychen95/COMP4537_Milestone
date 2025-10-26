826c3289cbff8f7747d1bf7b266c8ac3edc84dae
Wayne Chen <wchen206@my.bcit.ca>
2025-10-25 21:59:49 -0700

    updated css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,8 +11,8 @@ export default function Header() {
       <div className={styles.container}>
         <div className={styles.navInner}>
           <NavLink to="/" className={styles.logo}>
-            <BookOpen size={32}/>
-            <span>Web Computing Diary</span>
+            <BookOpen size={32} id="logo"/>
+            <span id={styles.title}>Wayne's Web Computing Diary</span>
           </NavLink>
 
           {/* Desktop Navigation */}
@@ -21,13 +21,11 @@ export default function Header() {
             <NavLink to="/about">About</NavLink>
           </div>
 
-          {/* Mobile Menu Button */}
           <button variant="ghost" size="icon" className={styles.mobileButton} onClick={() => setIsOpen(!isOpen)}>
             {isOpen ? <X className="h-5 w-5" /> : <Menu className="h-5 w-5" />}
           </button>
         </div>
 
-        {/* Mobile Navigation */}
         {isOpen && (
           <div className={styles.mobileNav}>
             <NavLink href="/topics" onClick={() => setIsOpen(false)}>Topics</NavLink>
